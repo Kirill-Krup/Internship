2875dd8fe1fefdab193425f2df4ebb7a52590c52
Kaegis <krupeto302@gmail.com>
2026-08-10 18:48:10 +0300

    added db schema for orderservice

---
+++ orderservice/src/main/java/com/internship/orderservice/model/Item.java
@@ -0,0 +1,26 @@
+package com.internship.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "items")
+@ToString
+public class Item extends AuditableEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private BigDecimal price;
+}
