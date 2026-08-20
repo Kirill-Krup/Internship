7c437cd775a53fd39688ef76b135d777b9b67dfb
Kaegis <krupeto302@gmail.com>
2026-08-20 19:50:29 +0300

    added testing

--- paymentservice/src/main/java/com/internship/paymentservice/model/Payment.java
+++ paymentservice/src/main/java/com/internship/paymentservice/model/Payment.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -37,6 +38,6 @@ public class Payment {
   @Indexed
   private Instant timestamp;
 
-  @Field("payment_amount")
+  @Field(value = "payment_amount", targetType = FieldType.DECIMAL128)
   private BigDecimal paymentAmount;
 }
